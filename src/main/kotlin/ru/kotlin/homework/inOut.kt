dae8d80ae112f5954c4bc363b5cd7f8b7e87aa88
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-25 17:03:44 +0200

    Contrvariant write group
    (cherry picked from commit ccf543765cb512544125bcb49101071cbe9fa820)

--- src/main/kotlin/ru/kotlin/homework/inOut.kt
+++ src/main/kotlin/ru/kotlin/homework/inOut.kt
@@ -10,15 +10,28 @@ fun main() {
     val readGroup: ReadGroup<Shape> = circleGroup
     val fromReadGroup: Shape = readGroup.fetch()
     println("From read group: $fromReadGroup")
+
+    val shapeGroup = Group<Shape>()
+    val writeCircleGroup: WriteGroup<Circle> = shapeGroup
+    writeCircleGroup.insert(Circle)
+    val writeTriangleGroup: WriteGroup<Triangle> = shapeGroup
+    writeTriangleGroup.insert(Triangle)
+
+    val fromShapeGroup: Shape = shapeGroup.fetch()
+    println("From shape group: $fromShapeGroup")
 }
 
 interface ReadGroup<out T> {
     fun fetch(): T
 }
 
-open class Group<T> : ReadGroup<T> {
+interface WriteGroup<in T> {
+    fun insert(item: T)
+}
+
+open class Group<T> : ReadGroup<T>, WriteGroup<T> {
     private val items: MutableList<T> = mutableListOf()
-    fun insert(item: T) {
+    override fun insert(item: T) {
         items.add(item)
     }
     override fun fetch(): T {
