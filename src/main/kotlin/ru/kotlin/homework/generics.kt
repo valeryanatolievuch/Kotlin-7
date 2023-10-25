72bce644496e89c1ea988d70a408a4adbd19a28a
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-25 09:33:22 +0200

    String array is not an Object array
    (cherry picked from commit 29634aeb0091118c8f4066840906cde2a85f81c5)

--- src/main/kotlin/ru/kotlin/homework/generics.kt
+++ src/main/kotlin/ru/kotlin/homework/generics.kt
@@ -5,5 +5,9 @@ package ru.kotlin.homework
 fun main() {
     val str: String = "Some string"
     val obj: Any = str
+
+    val strArray: Array<String> = arrayOf(str)
+    val objArray: Array<Any> = strArray
+
     println("String as object: $obj")
 }
