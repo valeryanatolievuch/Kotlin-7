41b355d14005f00f046de4cda2d3e5b4a6027fdf
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-25 09:30:07 +0200

    String is an object
    (cherry picked from commit 09b355af399eb9a265b4c57d138e4569365866ef)

---
+++ src/main/kotlin/ru/kotlin/homework/generics.kt
@@ -0,0 +1,9 @@
+@file:Suppress("RedundantExplicitType")
+
+package ru.kotlin.homework
+
+fun main() {
+    val str: String = "Some string"
+    val obj: Any = str
+    println("String as object: $obj")
+}
