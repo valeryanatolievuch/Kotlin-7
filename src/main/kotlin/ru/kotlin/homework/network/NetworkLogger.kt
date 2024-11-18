7c11b9b5f59416a65423ec48e6a103ff8817a8b5
Валерий Асланов <valeryanatolievuch@gmail.com>
2024-11-18 18:50:42 +0500

    Фикс ErrorLogger чтобы логгер работал с любыми типами ошибок и добавление метода dump() для возврата ошибок с временем.

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -2,28 +2,38 @@
 
 package ru.kotlin.homework.network
 
-import ru.kotlin.homework.Circle
-import java.lang.IllegalArgumentException
 import java.time.LocalDateTime
 
 /**
  * Известный вам список ошибок
  */
 sealed class ApiException(message: String) : Throwable(message) {
-    data object NotAuthorized : ApiException("Not authorized")
-    data object NetworkException : ApiException("Not connected")
-    data object UnknownException: ApiException("Unknown exception")
+    data object NotAuthorized : ApiException("Not authorized") {
+        private fun readResolve(): Any = NotAuthorized
+    }
+
+    data object NetworkException : ApiException("Not connected") {
+        private fun readResolve(): Any = NetworkException
+    }
+
+    data object UnknownException: ApiException("Unknown exception") {
+        private fun readResolve(): Any = UnknownException
+    }
 }
 
-class ErrorLogger<E : Throwable> {
+class ErrorLogger<in E : Throwable> {
 
-    val errors = mutableListOf<Pair<LocalDateTime, E>>()
+    private val errors = mutableListOf<Pair<LocalDateTime, Throwable>>()
 
-    fun log(response: NetworkResponse<*, E>) {
+    fun log(response: NetworkResponse<*, @UnsafeVariance E>) {
         if (response is Failure) {
             errors.add(response.responseDateTime to response.error)
         }
     }
+    fun dump(): List<Pair<LocalDateTime, Throwable>> {
+        return errors.toList()
+    }
+
 
     fun dumpLog() {
         errors.forEach { (date, error) ->
@@ -33,22 +43,12 @@ class ErrorLogger<E : Throwable> {
 }
 
 fun processThrowables(logger: ErrorLogger<Throwable>) {
-    logger.log(Success("Success"))
-    Thread.sleep(100)
-    logger.log(Success(Circle))
-    Thread.sleep(100)
     logger.log(Failure(IllegalArgumentException("Something unexpected")))
-
     logger.dumpLog()
 }
 
 fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
-    apiExceptionLogger.log(Success("Success"))
-    Thread.sleep(100)
-    apiExceptionLogger.log(Success(Circle))
-    Thread.sleep(100)
     apiExceptionLogger.log(Failure(ApiException.NetworkException))
-
     apiExceptionLogger.dumpLog()
 }
 
